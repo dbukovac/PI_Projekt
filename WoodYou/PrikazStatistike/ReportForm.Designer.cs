806af97edac721e9db330a3a3f818f0e2cbe6545
dbukovac <37124354+dbukovac@users.noreply.github.com>
2018-06-19 22:21:33 +0200

    napravljeni reporti za statistiku

--- WoodYou/PrikazStatistike/ReportForm.Designer.cs
+++ WoodYou/PrikazStatistike/ReportForm.Designer.cs
@@ -36,53 +36,35 @@
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource5 = new Microsoft.Reporting.WinForms.ReportDataSource();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource6 = new Microsoft.Reporting.WinForms.ReportDataSource();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource7 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource8 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource9 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource10 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource11 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.cboxIzbor = new System.Windows.Forms.ComboBox();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
             this.projektBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.fazaBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.materijalBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.korisnikBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.primkaBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.narudzbenicaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.partnerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.materijalBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.primkaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.stavkaprimkeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.stavkanarudzbeniceBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.narudzbenicaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tipkorisnikaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tippartneraBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.projektBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fazaBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.materijalBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.primkaBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.partnerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materijalBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.primkaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stavkaprimkeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stavkanarudzbeniceBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tipkorisnikaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tippartneraBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // reportViewer
-            // 
-            reportDataSource1.Name = "DataSetProjekt";
-            reportDataSource1.Value = this.projektBindingSource;
-            reportDataSource2.Name = "DataSetFaza";
-            reportDataSource2.Value = this.fazaBindingSource;
-            reportDataSource3.Name = "DataSetMaterijal";
-            reportDataSource3.Value = this.materijalBindingSource;
-            reportDataSource4.Name = "DataSetKorisnici";
-            reportDataSource4.Value = this.korisnikBindingSource;
-            reportDataSource5.Name = "DataSetPrimka";
-            reportDataSource5.Value = this.primkaBindingSource;
-            reportDataSource6.Name = "DataSetNarudzbenica";
-            reportDataSource6.Value = this.narudzbenicaBindingSource;
-            reportDataSource7.Name = "DataSetPartner";
-            reportDataSource7.Value = this.partnerBindingSource;
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource2);
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource3);
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource4);
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource5);
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource6);
-            this.reportViewer.LocalReport.DataSources.Add(reportDataSource7);
-            this.reportViewer.LocalReport.ReportEmbeddedResource = "PrikazStatistike.ReportProjekti.rdlc";
-            this.reportViewer.Location = new System.Drawing.Point(0, 31);
-            this.reportViewer.Name = "reportViewer";
-            this.reportViewer.ServerReport.BearerToken = null;
-            this.reportViewer.Size = new System.Drawing.Size(799, 419);
-            this.reportViewer.TabIndex = 0;
             // 
             // cboxIzbor
             // 
@@ -101,6 +83,48 @@
             this.cboxIzbor.TabIndex = 1;
             this.cboxIzbor.SelectedIndexChanged += new System.EventHandler(this.cboxIzbor_SelectedIndexChanged);
             // 
+            // reportViewer
+            // 
+            reportDataSource1.Name = "DataSetProjekt";
+            reportDataSource1.Value = this.projektBindingSource;
+            reportDataSource2.Name = "DataSetFaze";
+            reportDataSource2.Value = this.fazaBindingSource;
+            reportDataSource3.Name = "DataSetKorisnici";
+            reportDataSource3.Value = this.korisnikBindingSource;
+            reportDataSource4.Name = "DataSetPartner";
+            reportDataSource4.Value = this.partnerBindingSource;
+            reportDataSource5.Name = "DataSetMaterijal";
+            reportDataSource5.Value = this.materijalBindingSource;
+            reportDataSource6.Name = "DataSetPrimka";
+            reportDataSource6.Value = this.primkaBindingSource;
+            reportDataSource7.Name = "DataSetStavkaPrimke";
+            reportDataSource7.Value = this.stavkaprimkeBindingSource;
+            reportDataSource8.Name = "DataSetStavkaNarudzbenice";
+            reportDataSource8.Value = this.stavkanarudzbeniceBindingSource;
+            reportDataSource9.Name = "DataSetNarudzbenica";
+            reportDataSource9.Value = this.narudzbenicaBindingSource;
+            reportDataSource10.Name = "DataSetTipKorisnika";
+            reportDataSource10.Value = this.tipkorisnikaBindingSource;
+            reportDataSource11.Name = "DataSetTipPartnera";
+            reportDataSource11.Value = this.tippartneraBindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource5);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource6);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource7);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource8);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource9);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource10);
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource11);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "PrikazStatistike.ReportPrazni.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(12, 41);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(776, 246);
+            this.reportViewer.TabIndex = 2;
+            // 
             // projektBindingSource
             // 
             this.projektBindingSource.DataSource = typeof(PrikazStatistike.Projekt);
@@ -109,50 +133,68 @@
             // 
             this.fazaBindingSource.DataSource = typeof(PrikazStatistike.Faza);
             // 
-            // materijalBindingSource
-            // 
-            this.materijalBindingSource.DataSource = typeof(PrikazStatistike.Materijal);
-            // 
             // korisnikBindingSource
             // 
             this.korisnikBindingSource.DataSource = typeof(PrikazStatistike.Korisnik);
-            // 
-            // primkaBindingSource
-            // 
-            this.primkaBindingSource.DataSource = typeof(PrikazStatistike.Primka);
-            // 
-            // narudzbenicaBindingSource
-            // 
-            this.narudzbenicaBindingSource.DataSource = typeof(PrikazStatistike.Narudzbenica);
             // 
             // partnerBindingSource
             // 
             this.partnerBindingSource.DataSource = typeof(PrikazStatistike.Partner);
             // 
+            // materijalBindingSource
+            // 
+            this.materijalBindingSource.DataSource = typeof(PrikazStatistike.Materijal);
+            // 
+            // primkaBindingSource
+            // 
+            this.primkaBindingSource.DataSource = typeof(PrikazStatistike.Primka);
+            // 
+            // stavkaprimkeBindingSource
+            // 
+            this.stavkaprimkeBindingSource.DataSource = typeof(PrikazStatistike.Stavka_primke);
+            // 
+            // stavkanarudzbeniceBindingSource
+            // 
+            this.stavkanarudzbeniceBindingSource.DataSource = typeof(PrikazStatistike.Stavka_narudzbenice);
+            // 
+            // narudzbenicaBindingSource
+            // 
+            this.narudzbenicaBindingSource.DataSource = typeof(PrikazStatistike.Narudzbenica);
+            // 
+            // tipkorisnikaBindingSource
+            // 
+            this.tipkorisnikaBindingSource.DataSource = typeof(PrikazStatistike.Tip_korisnika);
+            // 
+            // tippartneraBindingSource
+            // 
+            this.tippartneraBindingSource.DataSource = typeof(PrikazStatistike.Tip_partnera);
+            // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cboxIzbor);
+            this.ClientSize = new System.Drawing.Size(800, 306);
             this.Controls.Add(this.reportViewer);
+            this.Controls.Add(this.cboxIzbor);
             this.Name = "ReportForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ReportForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.projektBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fazaBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.materijalBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.korisnikBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.primkaBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.partnerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materijalBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.primkaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stavkaprimkeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stavkanarudzbeniceBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.narudzbenicaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tipkorisnikaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tippartneraBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
         private System.Windows.Forms.ComboBox cboxIzbor;
         private System.Windows.Forms.BindingSource projektBindingSource;
         private System.Windows.Forms.BindingSource fazaBindingSource;
@@ -161,6 +203,11 @@
         private System.Windows.Forms.BindingSource primkaBindingSource;
         private System.Windows.Forms.BindingSource narudzbenicaBindingSource;
         private System.Windows.Forms.BindingSource partnerBindingSource;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.BindingSource stavkaprimkeBindingSource;
+        private System.Windows.Forms.BindingSource stavkanarudzbeniceBindingSource;
+        private System.Windows.Forms.BindingSource tipkorisnikaBindingSource;
+        private System.Windows.Forms.BindingSource tippartneraBindingSource;
     }
 }
 
--- WoodYou/PrikazStatistike/ReportForm.cs
+++ WoodYou/PrikazStatistike/ReportForm.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Linq;
+using System.Data.Entity;
 
 namespace PrikazStatistike
 {
@@ -17,8 +19,8 @@ namespace PrikazStatistike
         public BindingList<Faza> listaFaza = null;
         public BindingList<Materijal> listaMaterijala = null;
         public BindingList<Korisnik> listaKorisnika = null;
-        public BindingList<Primka> listaPrimka = null;
-        public BindingList<Narudzbenica> listaNarudzbenica = null;
+        public BindingList<Stavka_primke> listaStavkiPrimke = null;
+        public BindingList<Stavka_narudzbenice> listaStavkiNarudzbenica = null;
         public BindingList<Partner> listaPartnera= null;
 
         public ReportForm()
@@ -29,6 +31,7 @@ namespace PrikazStatistike
         private void ReportForm_Load(object sender, EventArgs e)
         {
             PopuniListe();
+            this.reportViewer.RefreshReport();
             this.reportViewer.RefreshReport();
         }
 
@@ -41,6 +44,9 @@ namespace PrikazStatistike
             narudzbenicaBindingSource.DataSource = null;
             korisnikBindingSource.DataSource = null;
             partnerBindingSource.DataSource = null;
+            tipkorisnikaBindingSource.DataSource = null;
+            stavkanarudzbeniceBindingSource.DataSource = null;
+            stavkaprimkeBindingSource.DataSource = null;
         }
 
         private void PopuniListe()
@@ -49,11 +55,11 @@ namespace PrikazStatistike
             {
                 listaProjekta = new BindingList<Projekt>(db.Projekt.ToList());
                 listaFaza = new BindingList<Faza>(db.Faza.ToList());
-                listaKorisnika = new BindingList<Korisnik>(db.Korisnik.ToList());
-                listaPrimka = new BindingList<Primka>(db.Primka.ToList());
+                listaKorisnika = new BindingList<Korisnik>(db.Korisnik.Include(x => x.Tip_korisnika).ToList());
+                listaStavkiPrimke = new BindingList<Stavka_primke>(db.Stavka_primke.Include(x => x.Primka).ToList());
                 listaMaterijala = new BindingList<Materijal>(db.Materijal.ToList());
-                listaNarudzbenica = new BindingList<Narudzbenica>(db.Narudzbenica.ToList());
-                listaPartnera = new BindingList<Partner>(db.Partner.ToList());
+                listaStavkiNarudzbenica = new BindingList<Stavka_narudzbenice>(db.Stavka_narudzbenice.Include(x => x.Narudzbenica).ToList());
+                listaPartnera = new BindingList<Partner>(db.Partner.Include(x => x.Tip_partnera1).ToList());
             }
         }
 
@@ -77,11 +83,7 @@ namespace PrikazStatistike
                 //RDC.Name = "DataSetProjekt";
                 //RDC.Value = this.projektBindingSource;
                 //this.reportViewer.LocalReport.DataSources.Add(RDC);
-                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportProjekti.rdlc";
-                this.reportViewer.RefreshReport();
-
-
-                
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportProjekti.rdlc";                
                 //this.reportViewer.LocalReport.ReportEmbeddedResource = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/Report.rdlc";
                 //this.reportViewer.RefreshReport();
             }
@@ -89,31 +91,79 @@ namespace PrikazStatistike
             {
                 ZatvoriSve();
                 fazaBindingSource.DataSource = listaFaza;
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportFaze.rdlc";
             }
             if (cboxIzbor.Text == "Materijal")
             {
                 ZatvoriSve();
                 materijalBindingSource.DataSource = listaMaterijala;
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportMaterijal.rdlc";
             }
             if (cboxIzbor.Text == "Primke")
             {
+                BindingList<Korisnik> korisniks = new BindingList<Korisnik>();
+                BindingList<Partner> partners = new BindingList<Partner>();
+                BindingList<Materijal> materijals = new BindingList<Materijal>();
+                BindingList<Primka> primkas = new BindingList<Primka>();
                 ZatvoriSve();
-                primkaBindingSource.DataSource = listaPrimka;
+                stavkaprimkeBindingSource.DataSource = listaStavkiPrimke;
+                foreach (var P in listaStavkiPrimke)
+                {
+                    korisniks.Add(P.Primka.Korisnik);
+                    partners.Add(P.Primka.Partner);
+                    primkas.Add(P.Primka);
+                    materijals.Add(P.Materijal);
+                }
+                korisnikBindingSource.DataSource = korisniks;
+                partnerBindingSource.DataSource = partners;
+                materijalBindingSource.DataSource = materijals;
+                primkaBindingSource.DataSource = primkas;
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportPrimke.rdlc";
             }
             if (cboxIzbor.Text == "Narudzbenice")
             {
+                BindingList<Korisnik> korisniks = new BindingList<Korisnik>();
+                BindingList<Partner> partners = new BindingList<Partner>();
+                BindingList<Materijal> materijals = new BindingList<Materijal>();
+                BindingList<Narudzbenica> narudzbenicas = new BindingList<Narudzbenica>();
                 ZatvoriSve();
-                narudzbenicaBindingSource.DataSource = listaNarudzbenica;
+                stavkanarudzbeniceBindingSource.DataSource = listaStavkiNarudzbenica;
+                foreach (var P in listaStavkiNarudzbenica)
+                {
+                    korisniks.Add(P.Narudzbenica.Korisnik);
+                    partners.Add(P.Narudzbenica.Partner);
+                    narudzbenicas.Add(P.Narudzbenica);
+                    materijals.Add(P.Materijal);
+                }
+                korisnikBindingSource.DataSource = korisniks;
+                partnerBindingSource.DataSource = partners;
+                materijalBindingSource.DataSource = materijals;
+                narudzbenicaBindingSource.DataSource = narudzbenicas;
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportNarudzbenice.rdlc";
             }
             if (cboxIzbor.Text == "Korisnici")
             {
+                BindingList<Tip_korisnika> tipovi = new BindingList<Tip_korisnika>();
                 ZatvoriSve();
                 korisnikBindingSource.DataSource = listaKorisnika;
+                foreach (var P in listaKorisnika)
+                {
+                    tipovi.Add(P.Tip_korisnika);
+                }
+                tipkorisnikaBindingSource.DataSource = tipovi;
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportKorisnici.rdlc";
             }
             if (cboxIzbor.Text == "Poslovni partneri")
             {
+                BindingList<Tip_partnera> tipovi = new BindingList<Tip_partnera>();
                 ZatvoriSve();
                 partnerBindingSource.DataSource = listaPartnera;
+                foreach (var P in listaPartnera)
+                {
+                    tipovi.Add(P.Tip_partnera1);
+                }
+                tippartneraBindingSource.DataSource = tipovi;
+                this.reportViewer.LocalReport.ReportPath = "C:/Users/Danko/Git/WoodYou/PrikazStatistike/ReportPartneri.rdlc";
             }
             this.reportViewer.RefreshReport();
         }
